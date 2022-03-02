46f592a29c254b0590031feda2176d27060744c4
ThanthongFifa <Fifa0962@gmail.com>
2022-03-02 09:50:07 +0700

    make new map

--- src/main/java/io/muzoo/ssc/zork/GameMap/Maps/map1.java
+++ src/main/java/io/muzoo/ssc/zork/GameMap/Maps/map1.java
@@ -16,20 +16,53 @@ public class map1 implements GameMap {
 
     @Override
     public void createMap(Game game) {
-        Room entrance = new Room("gate","you are in front of a gate of a dungeon");
-        Room secondroom = new Room("dungeon","it is dark in here");
+        //create room
+        Room room1 = new Room("Room1", "");
+        Room room2 = new Room("Room2", "");
+        Room room3 = new Room("Room3", "");
+        Room room4 = new Room("Room4", "");
+        Room room5 = new Room("Room5", "");
+        Room room6 = new Room("Room6", "");
+        Room room7 = new Room("Room7", "");
 
-        game.getMap().add(entrance);
-        game.getMap().add(secondroom);
+        //add room to game
+        game.getMap().add(room1);
+        game.getMap().add(room2);
+        game.getMap().add(room3);
+        game.getMap().add(room4);
+        game.getMap().add(room5);
+        game.getMap().add(room6);
+        game.getMap().add(room7);
 
-        entrance.setNextroom("north", secondroom);
-        entrance.setItem(new Potion());
+        //room1
+        room1.setNextroom("north", room4);
+        room1.setNextroom("east", room2);
 
-        secondroom.setNextroom("south", entrance);
-        secondroom.setMonster(new Skeleton(secondroom, null));
-        secondroom.setItem(new Axes());
+        //room2
+        room2.setNextroom("north", room3);
+        room2.setNextroom("west", room1);
+        room2.setNextroom("east", room6);
 
-        game.getPlayer().setLocation(entrance);
+        //room3
+        room3.setNextroom("west",room2);
+
+        //room4
+        room4.setNextroom("north", room5);
+        room4.setNextroom("south", room1);
+
+        //room5
+        room5.setNextroom("south", room4);
+        room5.setNextroom("west", room7);
+
+        //room6
+        room6.setNextroom("north", room7);
+        room6.setNextroom("west", room2);
+
+        //room7
+        room7.setNextroom("east", room5);
+        room7.setNextroom("south", room6);
+
+
     }
 
     @Override
@@ -40,18 +73,49 @@ public class map1 implements GameMap {
 
     @Override
     public void printMap(){
-        System.out.println("================= MAP =================");
-        System.out.println("                ---------             ");
-        System.out.println("                |       |             ");
-        System.out.println("                |       |             ");
-        System.out.println("                --|  |--              ");
-        System.out.println("                  |  |                ");
-        System.out.println("                  |  |                ");
-        System.out.println("                --|  |--              ");
-        System.out.println("                |       |             ");
-        System.out.println("                |       |             ");
-        System.out.println("                ---------             ");
-        System.out.println("=======================================");
+        System.out.println("    +-----------------------------+                                               +------------------------------------+\n" +
+                "    |                             |                                               |                                    |\n" +
+                "    |                             |                                               |                                    |\n" +
+                "    |                             |                                               |                                    |\n" +
+                "    |                             |                                               |                                    |\n" +
+                "    |                             |                                               |                                    |\n" +
+                "    |             5               +-----------------------------------------------+                                    |\n" +
+                "    |                             |                                               |                   7                |\n" +
+                "    |                             |                                               |                                    |\n" +
+                "    |                             |                                               |                                    |\n" +
+                "    |                             |                                               |                                    |\n" +
+                "    |                             |                                               |                                    |\n" +
+                "    +--------------+--------------+                                               |                                    |\n" +
+                "                   |                                                              +------------------------+-----------+\n" +
+                "                   |                                                                                       |\n" +
+                "+------------------+---------------------+                                                                 |\n" +
+                "|                                        |                                                          +------+------+\n" +
+                "|                                        |                                                          |             |\n" +
+                "|                                        |                                                          |             |\n" +
+                "|                                        |                                                          |             |\n" +
+                "|                                        |                                                          |     6       |\n" +
+                "|                   4                    |                                                          |             +----------+\n" +
+                "|                                        |          +-----------------------------+                 |             |          |\n" +
+                "|                                        |          |                             |                 |             |          |\n" +
+                "|                                        |          |                             |                 |             |          |\n" +
+                "|                                        |          |              3              +------+          +-------------+          |\n" +
+                "|                                        |          |                             |      |                                   |\n" +
+                "+---------------------+------------------+          |                             |      |                                   |\n" +
+                "                      |                             +-----------------------------+      |                                   |\n" +
+                "                      |                                                                  |                                   |\n" +
+                "                      |                                                                  |                                   |\n" +
+                "                      |                                                                  |                                   |\n" +
+                "                      |                                                                  |                                   |\n" +
+                "                      |                                                      +-----------+-------------------------+         |\n" +
+                "     +----------------+------------------+                                   |                                     |         |\n" +
+                "     |                                   |                                   |                                     |         |\n" +
+                "     |                                   |                                   |                                     |         |\n" +
+                "     |                                   +-----------------------------------+                2                    +---------+\n" +
+                "     |                1                  |                                   |                                     |\n" +
+                "     |                                   |                                   |                                     |\n" +
+                "     |                                   |                                   |                                     |\n" +
+                "     |                                   |                                   +-------------------------------------+\n" +
+                "     +-----------------------------------+");
     }
 
 }
